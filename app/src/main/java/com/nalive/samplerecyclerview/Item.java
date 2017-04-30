05f7fa2f35b5ceef39d3dd342dc78dea82e4939e
Ian <developer.kss@gmail.com>
2017-05-01 06:10:27 +0900

    images

---
+++ app/src/main/java/com/nalive/samplerecyclerview/Item.java
@@ -0,0 +1,21 @@
+package com.nalive.samplerecyclerview;
+
+public class Item {
+
+    int image;
+    String imagetitle;
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getImagetitle() {
+        return imagetitle;
+    }
+
+    public Item(int image, String imagetitle) {
+        this.image = image;
+        this.imagetitle = imagetitle;
+    }
+
+}
